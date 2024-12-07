7b955ca2e14ef188908da8c7a43273c5e6be9d8f
CheCarlois-User12 <anitaconstante@outlook.com>
2024-12-07 16:12:34 -0500

    Agregado correciones a los archivos staticos

--- BibliotecaStandFree/settings.py
+++ BibliotecaStandFree/settings.py
@@ -119,9 +119,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 # Ruta para la URL de archivos estáticos
-STATIC_ROOT = BASE_DIR / "staticfiles"
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
 
 MEDIA_ROOT = BASE_DIR / "media/"
 MEDIA_URL = '/media/'
